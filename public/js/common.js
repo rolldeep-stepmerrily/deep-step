ded0ac4b5ff56537eb4b1d8709cd5b928fec18cf
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-08-08 12:05:35 +0900

    update : signup page

--- public/js/common.js
+++ public/js/common.js
@@ -1,7 +1,73 @@
-function showElement(elementId) {
-  document.querySelector(`#${elementId}`).classList.remove('hidden');
-}
-
-function hideElement(elementId) {
+const hideElement = (elementId) => {
   document.querySelector(`#${elementId}`).classList.add('hidden');
-}
+};
+
+const showElement = (elementId) => {
+  document.querySelector(`#${elementId}`).classList.remove('hidden');
+};
+
+const validateUsername = (username) => {
+  if (!/^[a-zA-Z0-9]{4,16}$/.test(username)) {
+    alert('사용자 이름은 4~16자의 영문 대소문자와 숫자로만 입력해주세요.');
+
+    return false;
+  }
+
+  return true;
+};
+
+const validatePassword = (password, passwordConfirm) => {
+  if (password !== passwordConfirm) {
+    alert('비밀번호가 일치하지 않습니다.');
+
+    return false;
+  }
+
+  if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[`~!@#$%^&*()_=+])[A-Za-z\d`~!@#$%^&*()_=+]{8,16}$/.test(password)) {
+    alert('비밀번호는 8~16자의 영문 대소문자, 숫자, 특수문자로 입력해주세요.');
+
+    return false;
+  }
+
+  return true;
+};
+
+const validateEmail = (email) => {
+  if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+    alert('이메일 주소를 다시 확인해주세요.');
+
+    return false;
+  }
+
+  return true;
+};
+
+const validateName = (name) => {
+  if (!/^.{2,6}$/.test(name)) {
+    alert('이름은 2~6글자만 입력해주세요.');
+
+    return false;
+  }
+
+  return true;
+};
+
+const validateNickname = (nickname) => {
+  if (!/^[a-zA-Z0-9가-힣]{2,10}$/.test(nickname)) {
+    alert('닉네임은 2~10자의 영문, 숫자, 한글로만 입력해주세요.');
+
+    return false;
+  }
+
+  return true;
+};
+
+const validatePhoneNumber = (phoneNumber) => {
+  if (!/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/.test(phoneNumber)) {
+    alert('전화번호를 다시 확인해주세요.');
+
+    return false;
+  }
+
+  return true;
+};
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,96 @@
-function proceedToSignup() {
-  const serviceTerms = document.querySelector('#serviceTerms').checked;
-  const privacyTerms = document.querySelector('#privacyTerms').checked;
-  const privacyOptionTerms = document.querySelector('#privacyOptionTerms').checked;
-  const ageTerms = document.querySelector('#ageTerms').checked;
+const checkTerms = () => {
+  const serviceTerm = document.querySelector('#service-term').checked;
+  const privacyTerm = document.querySelector('#privacy-term').checked;
+  const ageTerm = document.querySelector('#age-term').checked;
 
-  if (!serviceTerms || !privacyTerms || !ageTerms) {
+  if (!serviceTerm || !privacyTerm || !ageTerm) {
     alert('필수 이용 약관에 동의해주세요.');
     return;
   }
 
   hideElement('terms-section');
   showElement('signup-section');
-}
+};
 
-function signup() {
-  console.log('회원가입 ㄱㄱ;;');
-}
+const signup = async () => {
+  const username = document.querySelector('#username').value.trim();
+
+  const isValidUsername = validateUsername(username);
+
+  if (!isValidUsername) {
+    return;
+  }
+
+  const password = document.querySelector('#password').value.trim();
+  const passwordConfirm = document.querySelector('#password-confirm').value.trim();
+
+  const isValidPassword = validatePassword(password, passwordConfirm);
+
+  if (!isValidPassword) {
+    return;
+  }
+
+  const email = document.querySelector('#email').value.trim();
+
+  const isValidEmail = validateEmail(email);
+
+  if (!isValidEmail) {
+    return;
+  }
+
+  const name = document.querySelector('#name').value.trim();
+
+  const isValidName = validateName(name);
+
+  if (!isValidName) {
+    return;
+  }
+
+  const nickname = document.querySelector('#nickname').value.trim();
+
+  const isValidNickname = validateNickname(nickname);
+
+  if (!isValidNickname) {
+    return;
+  }
+
+  const phoneNumber = document.querySelector('#phone-number').value.trim();
+
+  const isValidPhoneNumber = validatePhoneNumber(phoneNumber);
+
+  if (!isValidPhoneNumber) {
+    return;
+  }
+
+  const privacyOptionTerm = document.querySelector('#privacy-option-term').checked;
+
+  const signupPayload = {
+    username,
+    password,
+    email,
+    name,
+    nickname,
+    phoneNumber: phoneNumber.replaceAll('-', ''),
+    terms: {
+      isService: true,
+      isPrivacy: true,
+      isPrivacyOption: privacyOptionTerm,
+      isAge: true,
+    },
+  };
+
+  try {
+    const response = await axios.post('/api/users/signup', signupPayload);
+
+    console.log(response);
+  } catch (e) {
+    console.error(e);
+
+    alert('회원가입에 실패했습니다.');
+  }
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('proceed-button').addEventListener('click', proceedToSignup);
+  document.getElementById('proceed-button').addEventListener('click', checkTerms);
   document.getElementById('signup-button').addEventListener('click', signup);
 });

cbe49b47eb918d544c7c7e321d53cb966f102417
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-08-12 11:32:07 +0900

    debug : app.controller.ts  views/index.hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,8 @@ export class AppController {
   @Get()
   @Render('index')
   index() {
+    console.log(process.env.AWS_CLOUDFRONT_DOMAIN);
+    console.log(appLogo);
     return { appLogo, appName, styles: ['index'], scripts: ['index'] };
   }
 
